037d148ab7f9d24cee2619a0f9edfac7dadca77a
unknown <negi60239@gmail.com>
2024-05-13 21:16:27 +0530

    task4

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -52,7 +52,7 @@ const Dictionaryapp = () => {
 
           
 
-          <h3>Definition:</h3> {searchResult && {searchResult}}
+          <h3>Definition:</h3> {searchResult && <p>{searchResult}</p>}
     </div>
   )
 }
